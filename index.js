95b0457bdc02d9b22e2f15681e086d0613c722c0
YoYo-Joejo <joejo.velez21@gmail.com>
2021-05-11 21:11:57 -0400

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 
 let weekDay = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-let myFavoriteFood = ['chicken alfredo', 'sushi', 'pernil', ''];
+let myFavoriteFood = ['chicken alfredo', 'sushi', 'pernil', 'emplanadas'];
 
 if (myFavoriteFood[0]){
   console.log(myFavoriteFood[0]);
