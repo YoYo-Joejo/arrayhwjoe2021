aea5fa378b60c8c0b5b3f98196bda80a587d3599
YoYo-Joejo <joejo.velez21@gmail.com>
2021-05-11 20:49:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# arrayhwjoe2021
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/arrayhwjoe2021)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+let weekDay = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+let myFavoriteFood = ['chicken alfredo', 'sushi', 'pernil', ''];
+
+if (myFavoriteFood[0]){
+  console.log(myFavoriteFood[0]);
+}
+
