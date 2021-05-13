29018cde06524f2f8ed32c397791feecdd7f61a8
YoYo-Joejo <joejo.velez21@gmail.com>
2021-05-12 22:20:08 -0400

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ import './style.css';
 let weekDay = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 let myFavoriteFood = ['chicken alfredo', 'sushi', 'pernil', 'emplanadas'];
 
-if (myFavoriteFood[0]){
+
+if (myFavoriteFood == myFavoriteFood[0]){
   console.log(myFavoriteFood[0]);
 }
+  else if (myFavoriteFood = myFavoriteFood[1]) {
+    console.log(myFavoriteFood[1]);
+  }
+  else if (myFavoriteFood = myFavoriteFood[2]){
+    console.log(myFavoriteFood[2]);
+  }
+  else if (myFavoriteFood = myFavoriteFood[3]){
+    console.log(myFavoriteFood[3]);
+  }
 
+myFavoriteFood == myFavoriteFood[2];
